674ee88e3492e5befba6c7658467451f3f27e11d
realhesam <hesamehsanimoghaddam2004@gmail.com>
2025-08-09 11:43:01 +0430

    added TabularInLine to admin

--- products/admin.py
+++ products/admin.py
@@ -3,11 +3,20 @@ from django.contrib import admin
 from .models import Product, Comment
 
 
+class CommentsInLine(admin.TabularInline):
+    model = Comment
+    fields = ['author','text','stars','active',]
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title','price','description','active','datetime_modified',]
+    inlines = [
+        CommentsInLine,
+    ]
 
 
 @admin.register(Comment)
-class ProductAdmin(admin.ModelAdmin):
+class CommentAdmin(admin.ModelAdmin):
     list_display = ['author','product','text','stars','active','datetime_modified',]
