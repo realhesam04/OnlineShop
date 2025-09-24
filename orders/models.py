addd2ca9ac27c599f5031ddb1cde863e9f2e4e29
realhesam <hesamehsanimoghaddam2004@gmail.com>
2025-09-24 10:04:27 +0330

    add form for order app

--- orders/models.py
+++ orders/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    is_paid = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('User'))
+    is_paid = models.BooleanField(_('Is Paid?'),default=False)
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=700)
+    first_name = models.CharField(_('First Name'), max_length=100)
+    last_name = models.CharField(_('Last Name'), max_length=100)
+    phone_number = models.CharField(_('Phone Number'), max_length=15)
+    address = models.CharField(_('Address'), max_length=700)
 
-    order_notes = models.CharField(max_length=700, blank=True)
+    order_notes = models.CharField(_('Order Notes'), max_length=700, blank=True)
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(_('Created'), auto_now_add=True)
+    datetime_modified = models.DateTimeField(_('Modified'), auto_now=True)
 
     def __str__(self):
         return f'order {self.id}'
@@ -27,7 +28,4 @@ class OrderItem(models.Model):
     price = models.PositiveIntegerField()
 
     def __str__(self):
-        return f'OrderItem {self.id}: {self.product} X {self.quantity} (price:{self.price})'
-
-
-
+        return f'OrderItem {self.id}: {self.product} X {self.quantity}  (price:{self.price})'
